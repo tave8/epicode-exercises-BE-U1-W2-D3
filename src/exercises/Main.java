af367d15d9963f9bc343133433f32c01e291d308
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-03-18 15:14:02 +0100

    getExpensiveBooks done

--- src/exercises/Main.java
+++ src/exercises/Main.java
@@ -9,16 +9,22 @@ import enums.ProductCategory;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Main {
     static void main(String[] args) {
         // SAMPLE DATA
         Map<String, List> sampleData = getSampleData();
-        
+
+        // TODO: fix "unchecked assignment"
+        List<Product> products = sampleData.get("products");
+        List<Order> orders = sampleData.get("orders");
+        List<Customer> customers = sampleData.get("customers");
 
         // ****** EXERCISE 1:
         //      PRODUCT LIST: BOOK AND PRODUCT PRICE > 100
-        // List<Product> expensiveBooks = getExpensiveBooks();
+        List<Product> expensiveBooks = getExpensiveBooks(products);
+        System.out.println(expensiveBooks);
     }
 
 
@@ -34,7 +40,8 @@ public class Main {
         // products
         List<Product> products = List.of(
                 new Product(1, "book1", 12.34, ProductCategory.BOOK),
-                new Product(2, "book2", 12.34, ProductCategory.BOOK)
+                new Product(2, "book2", 12.34, ProductCategory.BOOK),
+                new Product(3, "book3", 102.34, ProductCategory.BOOK)
         );
 
         // customers
@@ -54,7 +61,10 @@ public class Main {
         );
     }
 
-    // static List<Product> getExpensiveBooks() {
-    //
-    // }
+    static List<Product> getExpensiveBooks(List<Product> products) {
+        Stream<Product> productsStream = products.stream();
+        Stream<Product> books = productsStream.filter(product -> product.getCategory().equals(ProductCategory.BOOK));
+        Stream<Product> expensiveBooks = books.filter(book -> book.getPrice() > 100);
+        return expensiveBooks.toList();
+    }
 }
